3992e84ec7e9c49341379fa451081615c9a6ce48
Ishika <ishika02908@gmail.com>
2024-07-01 11:57:03 +0530

    banner

---
+++ banner.py
@@ -0,0 +1,24 @@
+def printBanner():
+    banner ="""
+ ______          _    ______          _          
+ | ___ \        | |   | ___ \        | |         
+ | |_/ /__  _ __| |_  | |_/ / __ ___ | |__   ___ 
+ |  __/ _ \| '__| __| |  __/ '__/ _ \| '_ \ / _ \\
+ | | | (_) | |  | |_  | |  | | | (_) | |_) |  __/
+ \_|  \___/|_|   \__| \_|  |_|  \___/|_.__/ \___|
+                                                 
+                                                 
+"""
+
+    copyright_info = """
+-------------------------------------------------
+Port Probe - A Port Scanner
+Created by: Ishika Srivastava
+GitHub : https://github.com/ishika-srivastava/PortProbe
+Version: 1.0
+License: MIT
+(c) 2024 Ishika Srivastava. All rights reserved.
+-------------------------------------------------
+"""
+    print(banner)
+    print(copyright_info)
